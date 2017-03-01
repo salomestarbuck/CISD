c2884e73ba1aaa23026d074872acfefdcd6e41b7
Salomé <skstarbuck-dev@DOAETSJNU-TST01.soa.alaska.gov>
2017-02-28 16:28:30 -0900

    added fonts

--- wp-content/themes/creativeinsitedesign/header.php
+++ wp-content/themes/creativeinsitedesign/header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Roboto+Slab:100,300,400,700|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet"> 
 
 <?php wp_head(); ?>
 </head>
